d4a7c15a3904cbda1a84b534cfcb70b5e168a76a
TDWolff <torin.dean.wolff@gmail.com>
2025-03-18 14:20:58 -0700

    updating main.java for simpler outputs

--- src/main/java/com/torinwolff/spring/Main.java
+++ src/main/java/com/torinwolff/spring/Main.java
@@ -10,13 +10,5 @@ import org.springframework.context.ApplicationContext;
 public class Main {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Main.class, args);
-
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
 	}
 }
